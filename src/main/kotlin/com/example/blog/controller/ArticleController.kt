4adca5b20eef5c414f60807dfea925cc2d142c7c
nandy <ehdwls6856@gmail.com>
2024-10-27 22:29:55 +0900

    refactor repository names and auto format test

--- src/main/kotlin/com/example/blog/controller/ArticleController.kt
+++ src/main/kotlin/com/example/blog/controller/ArticleController.kt
@@ -2,19 +2,22 @@ package com.example.blog.controller
 
 
 import com.example.blog.repository.ArticleRepository
-import org.springframework.http.HttpStatus.*
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/article")
-class ArticleController(private val repository: ArticleRepository) {
+class ArticleController(private val articleRepository: ArticleRepository) {
 
     @GetMapping("/")
-    fun findAll() = repository.findAllByOrderByAddedAtDesc()
+    fun findAll() = articleRepository.findAllByOrderByAddedAtDesc()
 
     @GetMapping("/{slug}")
     fun findOne(@PathVariable slug: String) =
-        repository.findBySlug(slug) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
+        articleRepository.findBySlug(slug) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
 
 }
--- src/main/kotlin/com/example/blog/controller/UserController.kt
+++ src/main/kotlin/com/example/blog/controller/UserController.kt
@@ -1,18 +1,21 @@
 package com.example.blog.controller
 
 import com.example.blog.repository.UserRepository
-
-import org.springframework.http.HttpStatus.*
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/user")
-class UserController(private val repository: UserRepository) {
+class UserController(private val userRepository: UserRepository) {
 
     @GetMapping("/")
-    fun findAll() = repository.findAll()
+    fun findAll() = userRepository.findAll()
 
     @GetMapping("/{login}")
-    fun findOne(@PathVariable login: String) = repository.findByLogin(login) ?: throw ResponseStatusException(NOT_FOUND, "This user does not exist")
+    fun findOne(@PathVariable login: String) =
+        userRepository.findByLogin(login) ?: throw ResponseStatusException(NOT_FOUND, "This user does not exist")
 }
